c682258fd1e51a252710245a1c498eee608c44b5
Aadarsh Kumar Singh <aadarshkrsingh@outlook.com>
2020-05-20 11:15:22 -0700

    Fix bug in df sizes

--- lib.py
+++ lib.py
@@ -3,10 +3,10 @@ import pandas as pd
 from scipy.special import comb
 
 def build_price_df(init_price, up_mul, down_mul, num_iterations):
-    price_df = pd.DataFrame(np.zeros((num_iterations, num_iterations)))
+    price_df = pd.DataFrame(np.zeros((num_iterations + 1, num_iterations + 1)))
     price_df.loc[0][0] = init_price
 
-    for time_idx in range(1, num_iterations):
+    for time_idx in range(1, num_iterations + 1):
         for state_idx in range(time_idx + 1):
             price_df.iloc[state_idx, time_idx] = price_df.iloc[state_idx, time_idx - 1] * up_mul
 
